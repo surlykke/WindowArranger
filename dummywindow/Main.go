03327c34b8740ff7352b948cef992693d3f5ad8d
Christian Surlykke <christian@surlykke.dk>
2024-05-25 09:08:14 +0200

    Use glfw rather than fyne for dummywindow
    As we're just opening an empty window, fyne is a bit of an overkill

--- dummywindow/Main.go
+++ dummywindow/Main.go
@@ -1,19 +1,37 @@
+// Heavily based on the example on https://github.com/go-gl/glfw. 
+// That code is under a 3-clause bsd licence. See the file BSD in the root of this project
 package main
 
 import (
 	"log"
 	"os"
+	"runtime"
 
-	"fyne.io/fyne/v2/app"
+	"github.com/go-gl/glfw/v3.3/glfw"
 )
+
+func init() {
+	runtime.LockOSThread()
+}
 
 func main() {
 	if len(os.Args) != 2 {
 		log.Fatal("Expect one argument: Window title")
 	} else {
-		a := app.New()
-		w := a.NewWindow(os.Args[1])
 
-		w.ShowAndRun()
+		if err := glfw.Init(); err != nil {
+			panic(err)
+		} else {
+			defer glfw.Terminate()
+			if window, err := glfw.CreateWindow(640, 480, os.Args[1], nil, nil); err != nil {
+				panic(err)
+			} else {
+				window.MakeContextCurrent()
+				for !window.ShouldClose() {
+					window.SwapBuffers()
+					glfw.PollEvents()
+				}
+			}
+		}
 	}
 }
