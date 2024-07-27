d7079a94e6580b2ca4c0c0497f86860b61a06a0e
Christian Surlykke <christian@surlykke.dk>
2024-07-27 19:45:06 +0200

    Fix license stuff

---
+++ add-license-headers.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+cd $(dirname $0)
+
+for path in $(find . \( -name '*.go' -o -name '*.js' \) -not -name NamedColors.go -not -name scheme-handlers.go -not -exec grep -q GPL2 {} \; -print); do
+	tmp=`mktemp`
+
+cat<< EOF > $tmp
+// Copyright (c) Christian Surlykke
+//
+// This file is part of the WindowArranger project.
+// It is distributed under the GPL v2 license.
+// Please refer to the GPL2 file for a copy of the license.
+//
+EOF
+
+	cat $path >> $tmp
+	mv $tmp $path
+done
